084e775a9723f373ef33b213840175af77d667e5
Kristen Shiozaki <KristenShiozaki@Kristens-MacBook-Pro.local>
2017-10-31 16:39:12 -0400

    Updated File

--- BlackjackSimulation.py
+++ BlackjackSimulation.py
@@ -33,3 +33,34 @@ while(True):
     #terminate program: ctrl + z
     #set destination: "cd desktop"
     #open a file: python3 __(file name)__.py
+
+#10/31/2017
+#To upload a file as a github repository
+    #locate folder using: cd "folder name"
+    #once located the folder, write: git init
+    #to upload: git add .
+        # '.' uploads everything
+    #write: git status --> Tells you what files have been added
+    #write: git commit -m "__(insert name)__"
+        #e.g. git commit -m "first commit"
+    #To upload to the repository: git push --set-upstream "_(insert URL)_" master
+    
+#To download files to the folder you selected: git clone
+
+#Changing content of a file: 
+    #1. Locate Folder
+    #2. write: git status (if name is red & highlighted = means it is edited)
+    #3. git add .
+    #3. 
+    
+    
+    
+
+
+        
+        
+        
+        
+    
+
+    
